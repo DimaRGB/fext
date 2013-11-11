cc90492abd92b552e241f4c199c98ac3efeddd51
dima.rgb <dima.rgb@gmail.com>
2013-11-11 03:55:54 +0200

    method and other properties concats

--- fext.js
+++ fext.js
@@ -1,24 +1,37 @@
 ;(function(Function) {
 
-	Function.prototype.fext = function(bF) {
+	Function.prototype.concat = function(bF) {
 		var aF = this;
 		var aS = aF.toString();
 		var bS = bF.toString();
-
 		var aC = extractCode(aS);
 		var bC = extractCode(bS);
+		eval('var f = ' + wrapCode(aC + bC));
 
-		return wrapCode(aC + bC);
-	}
+		var aP = properties({}, aF);
+		var bP = properties({}, bF);
 
-	function extractCode(fS) {
-		var i1 = fS.indexOf('{') + 1;
-		var i2 = fS.indexOf('}');
-		return fS.slice(i1, i2).trim();
-	}
+		f = properties(f, aP);
+		f = properties(f, bP);
 
-	function wrapCode(fC) {
-		return 'function() {' + fC + '}';
+		return f;
+
+		// support functions
+		function extractCode(fS) {
+			var i1 = fS.indexOf('{') + 1;
+			var i2 = fS.indexOf('}');
+			return fS.slice(i1, i2);
+		}
+
+		function wrapCode(fC) {
+			return 'function() {' + fC + '}';
+		}
+
+		function properties(a, b) {
+			for( var key in b )
+				a[key] = b[key];
+			return a;
+		}
 	}
 
 })(Function);
